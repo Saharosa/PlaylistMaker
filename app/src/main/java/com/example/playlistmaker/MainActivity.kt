e9323f4d8b2b9e3a4624802b4b00b4794aec6590
klyko <klykovskiyt@mail.ru>
2025-07-17 14:42:31 +0300

    закончена верстка, добавлен в манифест активити настроек

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,5 +18,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val buttonSettings = findViewById<Button>(R.id.settings)
+        buttonSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+        }
+        val buttonMedia = findViewById<Button>(R.id.media)
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonMedia.setOnClickListener(buttonClickListener)
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        buttonSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+        }
     }
-}
+    }
