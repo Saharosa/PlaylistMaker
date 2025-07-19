4c0e5b7f137c5238b5c3296942cd21c3bd8ef627
klyko <klykovskiyt@mail.ru>
2025-07-19 20:07:29 +0300

    реализованы кнопки экрана настроек, помимо смены ночной темы

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -21,5 +23,29 @@ class SettingsActivity : AppCompatActivity() {
             val displayIntent = Intent(this, SearchActivity::class.java)
             startActivity(displayIntent)
         }
+        val buttonSupport = findViewById<Button>(R.id.support)
+        buttonSupport.setOnClickListener{
+            val message = "Привет, Android-разработка — это круто!"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("yourEmail@ya.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+        val buttonShare = findViewById<Button>(R.id.buttonShare)
+        buttonShare.setOnClickListener {
+            val message = "https://practicum.yandex.com/android-developer/"
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(Intent.createChooser(shareIntent, "Поделиться через:"))
+        }
+        val buttonUserAgreement = findViewById<Button>(R.id.userAgreement)
+        buttonUserAgreement.setOnClickListener{
+            val url = "https://yandex.ru/legal/practicum_offer/"
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(browserIntent)
+        }
+
     }
 }
