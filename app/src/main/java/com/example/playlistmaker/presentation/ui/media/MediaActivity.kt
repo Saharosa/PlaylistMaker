d8d5a5b6562f7d0cca26b8889837e9e3ea522cba
klyko <klykovskiyt@mail.ru>
2025-11-19 23:33:40 +0300

    экран медиатека

--- app/src/main/java/com/example/playlistmaker/presentation/ui/media/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/ui/media/MediaActivity.kt
@@ -29,8 +29,8 @@ class MediaActivity : AppCompatActivity() {
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
         tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
-                0 -> tab.text = "Избранные треки"
-                1 -> tab.text = "Плейлисты"
+                0 -> tab.text = getString(R.string.favourite)
+                1 -> tab.text = getString(R.string.playlist)
             }
         }
         tabMediator.attach()
